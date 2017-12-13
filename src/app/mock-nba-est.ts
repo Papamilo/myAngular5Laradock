416db9c8646c02406c79fa001f195465bebabdfb
Emile <ecantero@team-altersolutions.com>
2017-12-13 17:14:53 +0100

    displayList

---
+++ src/app/mock-nba-est.ts
@@ -0,0 +1,19 @@
+import { Nba } from './nba';
+
+export const NBAE: Nba[] = [
+  { id: 1, name: 'Boston Celtics' },
+  { id: 2, name: 'Cleveland Cavaliers' },
+  { id: 3, name: 'Toronto Raptors' },
+  { id: 4, name: 'Milwaukee Bucks' },
+  { id: 5, name: 'Indiana Pacers' },
+  { id: 6, name: 'Washington Wizards' },
+  { id: 7, name: 'Philadelphia 76ers' },
+  { id: 8, name: 'Detroit Pistons' },
+  { id: 9, name: 'New York Knicks' },
+  { id: 10, name: 'Miami Heat' },
+  { id: 11, name: 'Brooklin Nets' },
+  { id: 12, name: 'Orlando Magic' },
+  { id: 13, name: 'Charlotte Hornets' },
+  { id: 14, name: 'Chicago Bulls' },
+  { id: 15, name: 'Atlanta Hawks' }
+];
---
+++ src/app/nba.ts
@@ -0,0 +1,8 @@
+export class Nba {
+  id: number;
+  name: string;
+  rank: number;
+  team: string;
+  victories: number;
+  defeats: number;
+}
