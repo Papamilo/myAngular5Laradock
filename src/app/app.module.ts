3ed59b7d5f9b45058aaa798b6a6ca1ad1c4a2ceb
cantero <emile.cantero@gmail.com>
2017-12-16 18:09:56 +0100

    nba selected est teams finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { NbaComponent } from './nba/nba.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    NbaComponent
+    NbaComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
