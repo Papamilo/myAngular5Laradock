d82e811435753e99f923eb52a02365a6b66e4ab0
Emile <ecantero@team-altersolutions.com>
2017-12-19 18:16:04 +0100

    updated sevice nba

--- src/app/nba-message.service.ts
+++ src/app/nba-message.service.ts
@@ -3,9 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class NbaMessageService {
 
-  nbaMessages: string[] = [];
+  nbaMessages: any[] = [];
+  // To be modify with the write type under and bellow too
 
-  add(nbaMessage: string) {
+  add(nbaMessage: any[]) {
     this.nbaMessages.push(nbaMessage);
   }
 
--- src/app/nba-messages/nba-messages.component.ts
+++ src/app/nba-messages/nba-messages.component.ts
@@ -1,3 +1,4 @@
+import { NbaMessageService } from './../nba-message.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NbaMessagesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public nbaMessageService: NbaMessageService) { }
 
   ngOnInit() {
   }
